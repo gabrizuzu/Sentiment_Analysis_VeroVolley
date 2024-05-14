9bed1233e3896429939b05f31b9cf49cb99ca30f
clapcibus <cateclapci@gmail.com>
2024-05-14 10:32:03 +0200

    sentiment_total update + add images

--- official_statistics/sentiment_total.py
+++ official_statistics/sentiment_total.py
@@ -3,6 +3,13 @@ import locale
 import re
 from dateutil import parser
 from datetime import datetime
+import matplotlib.pyplot as plt
+from matplotlib.dates import date2num
+
+def load_data(file_name):
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+    return data
 
 def add_year_to_date(data):
     # 1) VERIFICA SE NELLA STRINGA È PRESENTE L'ANNO => 
@@ -88,118 +95,97 @@ def format_data(data):
             # Esempio:
             # 5 giu 2023
             return datetime.strptime(data, '%d %b %Y')
-    
-#OPEN FILE
-with open('/Users/clapcibus/Downloads/sentiment_total.json', 'r') as f:
-    data = json.load(f)
-    
-arrayTot = []
 
-for post in data:
-    platform = post.get('platform')
+# Sentiment statistics
+# GENERAL
+# POSTS
+def count_sentiment_posts(posts):
+    count = {
+        2021: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12},
+        2022: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12},
+        2023: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12},
+        2024: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12}
+    }
+
+    for post in posts:
+        anno = post['anno']
+        mese = post['mese']
+        sentiment = post['sentiment_post']
+
+        if anno in count:
+            if sentiment == 'positive': count[anno]['positive'][mese - 1] += 1
+            elif sentiment == 'negative': count[anno]['negative'][mese - 1] += 1
+            else: count[anno]['neutral'][mese - 1] += 1
+
+    return count
     
-    #INSTAGRAM 
-    if platform == 'IG':
-        data_pubblicazione = format_data(post['taken_at_date'])
-        
-        postSing = {
-            'platform' : platform,
-            'source': post['source'],
-            'giorno': data_pubblicazione.day,
-            'mese': data_pubblicazione.month,
-            'anno': data_pubblicazione.year,
-            'sentiment_post': post['sentiment'],
-            'nr_like': post['likes_count'],
-            'nr_comment' : post['comments_count'],
-            'comments': []
-        }
-        
+# COMMENTS
+'''
+def count_sentiment_comments(posts):
+    count = {
+        2021: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12},
+        2022: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12},
+        2023: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12},
+        2024: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12}
+    }
+
+    for post in posts:
         comments = post.get('comments', [])
         for comment in comments:
-            data_comm = format_data(comment['created_at_utc'])
-            
-            comm = {
-                'author': comment['username'],
-                'giorno': data_comm.day,
-                'mese': data_comm.month,
-                'anno': data_comm.year,
-                'sentiment_comment': comment['sentiment'],
-                'nr_like' : comment['like_count']
-            }
-            
-            postSing['comments'].append(comm)
-    
-        arrayTot.append(postSing)
-   
-    #FACEBOOK
-    elif platform == 'FB':
-        data_pubblicazione = format_data(post['date'])
+            if comment is not None:
+                if "verovolley" not in comment.get('author'):
+                    anno = comment.get('anno')
+                    mese = comment.get('mese')
+                    sentiment = comment.get('sentiment_comment')
 
-        postSing = {
-            'platform' : platform,
-            'source': post['source'],
-            'giorno': data_pubblicazione.day,
-            'mese': data_pubblicazione.month,
-            'anno': data_pubblicazione.year,
-            'sentiment_post': post['sentiment'],
-            'nr_like': post['num_likes'],
-            'nr_comment' : post['num_comments'],
-            'comments': []
-        }
-        
-        comments = post.get('comments', [])
-        for comment in comments:
-            data_comm = format_data(comment['date'])
-            
-            comm = {
-                'author': comment['author'],
-                'giorno': data_comm.day,
-                'mese': data_comm.month,
-                'anno': data_comm.year,
-                'sentiment_comment': comment['sentiment'],
-                'nr_like' : comment['likes_num']
-            }
-            
-            postSing['comments'].append(comm)
-    
-        arrayTot.append(postSing)
-    
-    #WEB
-    elif platform == 'Web':
-        data_pubblicazione = format_data(post['date'])
-        
-        postSing = {
-            'platform' : platform,
-            'source': post['source'],
-            'giorno': data_pubblicazione.day,
-            'mese': data_pubblicazione.month,
-            'anno': data_pubblicazione.year,
-            'sentiment_post': post['sentiment'],
-            'comments': []
-        }
-        
-        '''
-        #!!!! da completare tutti i post del web per ora non hanno commenti !!!!
-        comments = post.get('comments', [])
-        
-        for comment in comments:
-            data_comm = parse_data(comment['created_at_utc'])
-            
-            comm = {
-                'author': comment['author'],
-                'giorno': data_comm.day,
-                'mese': data_comm.month,
-                'anno': data_comm.year,
-                'sentiment_comment': comment['sentiment'],
-                'nr_like' : comment['likes_num']
-            }
-            
-            postSing['comments'].append(comm)
-        '''
-        postSing['comments'].append(comments)
-        arrayTot.append(postSing)    
+                    if anno in count:
+                        if sentiment == 'positive': count[anno]['positive'][mese - 1] += 1
+                        elif sentiment == 'negative': count[anno]['negative'][mese - 1] += 1
+                        else: count[anno]['neutral'][mese - 1] += 1
 
-# ora arrayTot contiene tutti i dati scritti nel formato: 
+    return count
+'''
+
+# SPECIFIC
+def count_sentiment_posts_specific(posts, platform):
+    count = {
+        2021: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12},
+        2022: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12},
+        2023: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12},
+        2024: {'positive': [0] * 12, 'negative': [0] * 12, 'neutral': [0] * 12}
+    }
+
+    for post in posts:
+        if post['platform'] == platform:
+            anno = post['anno']
+            mese = post['mese']
+            sentiment = post['sentiment_post']
+
+            if anno in count:
+                if sentiment == 'positive': count[anno]['positive'][mese - 1] += 1
+                elif sentiment == 'negative': count[anno]['negative'][mese - 1] += 1
+                else: count[anno]['neutral'][mese - 1] += 1
+
+    return count
+
+# Plot the statistics 
+def plot_bar_chart(title, data):
+    months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    colors = ['green', 'red', 'blue', 'yellow', 'orange', 'purple', 'pink', 'cyan', 'magenta', 'brown', 'grey', 'black']
+    bars = plt.bar(months, data, color=colors)
+    for bar, value in zip(bars, data):
+        plt.text(bar.get_x() + bar.get_width()/2, bar.get_height(), value, ha='center', va='bottom', fontsize=8, color='black', weight='bold')
+    
+    plt.xlabel('Months', fontsize=10, weight='bold')
+    plt.ylabel('Value of sentiment', fontsize=10, weight='bold')
+    plt.title(title, fontsize=14)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig(f'{title}.png')
+    plt.show()
+
+
+# OTTENIAMO UN UNICO ARRAY FORMATTATO NEL SEGUENTE MODO:
 #               - platform
 #               - source
 #               - giorno
@@ -216,149 +202,186 @@ for post in data:
 #                   - anno
 #                   - sentiment_comment
 #                   - nr_like    
+def process_post(post):  
+    platform = post['platform']
+        
+    #INSTAGRAM 
+    if platform == 'IG':
+        data_pubblicazione = format_data(post['taken_at_date'])
+        
+        if 'verovolley' in post['source']:
+            postSing = {
+                'platform' : platform,
+                'source': post['source'],
+                'giorno': data_pubblicazione.day,
+                'mese': data_pubblicazione.month,
+                'anno': data_pubblicazione.year,
+                'sentiment_post': 'none',
+                'nr_like': post['likes_count'],
+                'nr_comment' : post['comments_count'],
+                'comments': []
+            }
+        else:
+            postSing = {
+                'platform' : platform,
+                'source': post['source'],
+                'giorno': data_pubblicazione.day,
+                'mese': data_pubblicazione.month,
+                'anno': data_pubblicazione.year,
+                'sentiment_post': post['sentiment'],
+                'nr_like': post['likes_count'],
+                'nr_comment' : post['comments_count'],
+                'comments': []
+            }
+            
+        for comment in post.get('comments', []):
+            data_comm = format_data(comment['created_at_utc'])
+            
+            comm = {
+                'author': comment['username'],
+                'giorno': data_comm.day,
+                'mese': data_comm.month,
+                'anno': data_comm.year,
+                'sentiment_comment': comment['sentiment'],
+                'nr_like' : comment['like_count']
+            }
+            
+            postSing['comments'].append(comm)
+        
+                
+    #FACEBOOK
+    elif platform == 'FB':
+        data_pubblicazione = format_data(post['date'])
 
-
-# ----- POST -----
-count_pos = [0] * 12
-count_neg = [0] * 12
-count_neu = [0] * 12
-count_pos_2 = [0] * 12
-count_neg_2 = [0] * 12
-count_neu_2 = [0] * 12
-count_pos_3 = [0] * 12
-count_neg_3 = [0] * 12
-count_neu_3 = [0] * 12
-count_pos_4 = [0] * 12
-count_neg_4 = [0] * 12
-count_neu_4 = [0] * 12
-
-count_pos_v = [0] * 12
-count_neg_v = [0] * 12
-count_neu_v = [0] * 12
-count_pos_2_v = [0] * 12
-count_neg_2_v = [0] * 12
-count_neu_2_v = [0] * 12
-count_pos_3_v = [0] * 12
-count_neg_3_v = [0] * 12
-count_neu_3_v = [0] * 12
-count_pos_4_v = [0] * 12
-count_neg_4_v = [0] * 12
-count_neu_4_v = [0] * 12
-
-count_post = 0
-
-# STATISTICHE GENERALI
-for post in arrayTot:
-    anno = post['anno']
-    mese = post['mese']
-    sentiment = post['sentiment_post']
+        if 'verovolley' in post['source']:
+            postSing = {
+                'platform' : platform,
+                'source': post['source'],
+                'giorno': data_pubblicazione.day,
+                'mese': data_pubblicazione.month,
+                'anno': data_pubblicazione.year,
+                'sentiment_post': 'none',
+                'nr_like': post['num_likes'],
+                'nr_comment' : post['num_comments'],
+                'comments': []
+            }
+        
+        else:
+            postSing = {
+                'platform' : platform,
+                'source': post['source'],
+                'giorno': data_pubblicazione.day,
+                'mese': data_pubblicazione.month,
+                'anno': data_pubblicazione.year,
+                'sentiment_post': post['sentiment'],
+                'nr_like': post['num_likes'],
+                'nr_comment' : post['num_comments'],
+                'comments': []
+            }
+        
+        for comment in post.get('comments', []):
+            data_comm = format_data(comment['date'])
+            
+            comm = {
+                'author': comment['author'],
+                'giorno': data_comm.day,
+                'mese': data_comm.month,
+                'anno': data_comm.year,
+                'sentiment_comment': comment['sentiment'],
+                'nr_like' : comment['likes_num']
+            }
+            
+            postSing['comments'].append(comm)
     
-    #STATICHE POST SOLO DI VEROVOLLEY
-    if 'verovolley' in post['source']:
-        if anno == 2024:
-            count_list = count_pos_v if sentiment == 'positive' else (count_neg_v if sentiment == 'negative' else count_neu_v)
-            count_list[mese - 1] += 1
+    #WEB
+    elif platform == 'Web':
+        data_pubblicazione = format_data(post['date'])
+        
+        if 'verovolley' in post['source']:   
+            postSing = {
+                'platform' : platform,
+                'source': post['source'],
+                'giorno': data_pubblicazione.day,
+                'mese': data_pubblicazione.month,
+                'anno': data_pubblicazione.year,
+                'sentiment_post': 'none',
+                'comments': []
+            }
+        
+        else:
+            postSing = {
+                'platform' : platform,
+                'source': post['source'],
+                'giorno': data_pubblicazione.day,
+                'mese': data_pubblicazione.month,
+                'anno': data_pubblicazione.year,
+                'sentiment_post': post['sentiment'],
+                'comments': []
+            }
+        
+        #!!!! da completare tutti i post del web per ora non hanno commenti !!!!
+        comments = post.get('comments', [])
+        
+        '''
+        for comment in comments:
+            data_comm = parse_data(comment['created_at_utc'])
+            
+            comm = {
+                'author': comment['author'],
+                'giorno': data_comm.day,
+                'mese': data_comm.month,
+                'anno': data_comm.year,
+                'sentiment_comment': comment['sentiment'],
+                'nr_like' : comment['likes_num']
+            }
+            
+            postSing['comments'].append(comm)
+        '''
+        postSing['comments'].append(comments)
+    
+    return postSing 
 
-        elif anno == 2023:
-            count_list = count_pos_2_v if sentiment == 'positive' else (count_neg_2_v if sentiment == 'negative' else count_neu_2_v)
-            count_list[mese - 1] += 1
-        
-        elif anno == 2022:
-            count_list = count_pos_3_v if sentiment == 'positive' else (count_neg_3_v if sentiment == 'negative' else count_neu_3_v)
-            count_list[mese - 1] += 1
-        
-        elif anno == 2021:
-            count_list = count_pos_4_v if sentiment == 'positive' else (count_neg_4_v if sentiment == 'negative' else count_neu_4_v)
-            count_list[mese - 1] += 1
-        
-        count_post+=1
+def main():
+    file_path = '/Users/clapcibus/Downloads/sentiment_total.json'
+    data = load_data(file_path)
+    processed_posts = [process_post(post) for post in data]
+    counts_post = count_sentiment_posts(processed_posts)
    
-    #STATISTICHE POST SENZA VEROVOLLEY
-    else:
-        if anno == 2024:
-            count_list = count_pos if sentiment == 'positive' else (count_neg if sentiment == 'negative' else count_neu)
-            count_list[mese - 1] += 1
+    # Conteggio dei post per la piattaforma specifica
+    platforms = ['IG', 'FB', 'Web']
+    counts_posts_specific = {}
+    for platform in platforms:
+        counts_posts_specific[platform] = count_sentiment_posts_specific(processed_posts, platform)
 
-        elif anno == 2023:
-            count_list = count_pos_2 if sentiment == 'positive' else (count_neg_2 if sentiment == 'negative' else count_neu_2)
-            count_list[mese - 1] += 1
-        
-        elif anno == 2022:
-            count_list = count_pos_3 if sentiment == 'positive' else (count_neg_3 if sentiment == 'negative' else count_neu_3)
-            count_list[mese - 1] += 1
-        
-        elif anno == 2021:
-            count_list = count_pos_4 if sentiment == 'positive' else (count_neg_4 if sentiment == 'negative' else count_neu_4)
-            count_list[mese - 1] += 1
-
-    count_post += 1
-        
-print("NUMBER OF POST ", count_post)
-print("")
-print("2024")
-print("Positive ", count_pos)
-print("Negative ", count_neg)
-print("Neutral ", count_neu)
-print("")
-print("2023")
-print("Positive ", count_pos_2)
-print("Negative ", count_neg_2)
-print("Neutral ", count_neu_2)
-print("")
-print("2022")
-print("Positive ", count_pos_3)
-print("Negative ", count_neg_3)
-print("Neutral ", count_neu_3)
-print("")
-print("2021")
-print("Positive ", count_pos_4)
-print("Negative ", count_neg_4)
-print("Neutral ", count_neu_4)
-print("")
-print("VEROVOLLEY")
-print("2024")
-print("Positive ", count_pos_v)
-print("Negative ", count_neg_v)
-print("Neutral ", count_neu_v)
-print("")
-print("2023")
-print("Positive ", count_pos_2_v)
-print("Negative ", count_neg_2_v)
-print("Neutral ", count_neu_2_v)
-print("")
-print("2022")
-print("Positive ", count_pos_3_v)
-print("Negative ", count_neg_3_v)
-print("Neutral ", count_neu_3_v)
-print("")
-print("2021")
-print("Positive ", count_pos_4_v)
-print("Negative ", count_neg_4_v)
-print("Neutral ", count_neu_4_v)
-
-
-'''
-# ----- COMMENTS -----
-for post in data:
-    anno = post['anno']
-    mese = post['mese']
-    sentiment = post['sentiment']
-
-    if anno == 2024:
-        count_list = count_pos if sentiment == 'positive' else (count_neg if sentiment == 'negative' else count_neu)
-        count_list[mese - 1] += 1
-
-    elif anno == 2023:
-        count_list = count_pos_2 if sentiment == 'positive' else (count_neg_2 if sentiment == 'negative' else count_neu_2)
-        count_list[mese - 1] += 1
+    # Statistiche generali
+    for year, count_data in counts_post.items():
+        plot_bar_chart(f'General statistics of posts: positive sentiment - year {year}', count_data['positive'])
+        plot_bar_chart(f'General statistics of posts: negative sentiment - year {year}', count_data['negative'])
+        plot_bar_chart(f'General statistics of posts: neutral sentiment - year {year}', count_data['neutral'])
     
-    elif anno == 2022:
-        count_list = count_pos_3 if sentiment == 'positive' else (count_neg_3 if sentiment == 'negative' else count_neu_3)
-        count_list[mese - 1] += 1
+    #counts_comments = count_sentiment_comments(processed_posts)
     
-    elif anno == 2021:
-        count_list = count_pos_4 if sentiment == 'positive' else (count_neg_4 if sentiment == 'negative' else count_neu_4)
-        count_list[mese - 1] += 1
-
-    count_post += 1
-'''
+    # GENERAL STATISTICS
+    for year, count_data in counts_post.items():
+        plot_bar_chart(f'General statistics of posts: positive sentiment - year {year}', count_data['positive'])
+        plot_bar_chart(f'General statistics of posts: negative sentiment - year {year}', count_data['negative'])
+        plot_bar_chart(f'General statistics of posts: neutral sentiment - year {year}', count_data['neutral'])
+        
+    # SPECIFIC STATISTICS FOR EACH PLATFORM
+    for platform, counts in counts_posts_specific.items():
+        for year, count_data in counts.items():
+            plot_bar_chart(f'{platform} statistics of posts: positive sentiment - year {year}', count_data['positive'])
+            plot_bar_chart(f'{platform} statistics of posts: negative sentiment - year {year}', count_data['negative'])
+            plot_bar_chart(f'{platform} statistics of posts: neutral sentiment - year {year}', count_data['neutral'])
+    
+    '''for year, count_data in counts_comments.items():
+        plot_bar_chart(year, f'General statistics of comments: positive sentiment - year {year}', count_data['positive'])
+        plot_bar_chart(year, f'General statistics of comments: negative sentiment - year {year}', count_data['negative'])
+        plot_bar_chart(year, f'General statistics of comments: neutral sentiment - year {year}', count_data['neutral'])
+    '''
+    
+    #SPECIFIC STATISTICS
+    
+    
+if __name__ == "__main__":
+    main()
