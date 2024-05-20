d203f2460626b9a0dde5e21bbfd90bb313e65aac
Ignacio <nachobascar@hotmail.com>
2024-05-20 14:56:39 +0200

    Add dashboard page

--- Recharts/src/index.js
+++ Recharts/src/index.js
@@ -1,13 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App";
+import VeroVolley from "./VeroVolley";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/vero-volley",
+    element: <VeroVolley />,
+  },
+]);
+
 root.render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 );
