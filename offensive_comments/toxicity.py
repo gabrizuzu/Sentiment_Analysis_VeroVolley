705e1011d5084d0b8c59e487e1866396299849dc
gabrizuzu <zuzugabriele24@gmail.com>
2024-05-21 17:58:11 +0200

    offensive comments

--- offensive_comments/toxicity.py
+++ offensive_comments/toxicity.py
@@ -3,6 +3,7 @@ import requests
 import csv
 import time
 import pandas as pd
+from tqdm import tqdm
 
 def get_sources(file_path):
     with open(file_path, 'r') as f:
@@ -21,18 +22,20 @@ def analyze_comment(api_key, comment):
     response = requests.post(url, params={'key': api_key}, json=data)
     return response.json()
 
-def add_toxicity_to_comments(file_path, api_key, source):
+def add_toxicity_to_comments(file_path, api_key):
     with open(file_path, 'r') as f:
         data = json.load(f)
 
-    csv_file_path = f'offensive_comments/csv_output/{source}_comments_with_toxicity.csv'
+    csv_file_path = f'offensive_comments/csv_output/comments_with_toxicity.csv'
 
     with open(csv_file_path, 'w', newline='') as csvfile:
         fieldnames = ['text', 'toxicity', 'severe_toxicity', 'identity_attack', 'insult', 'total_toxicity']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         writer.writeheader()
+        total_comments = sum(1 for item in data if item['source'].startswith('verovolley') for comment in item['comments'] if comment['sentiment'] == 'negative' and 'toxicity' not in comment)
 
+        pbar = tqdm(total=total_comments)
         for item in data:
             if item['source'].startswith('verovolley'):
                 for comment in item['comments']:
@@ -44,7 +47,8 @@ def add_toxicity_to_comments(file_path, api_key, source):
                         comment['insult'] = analysis['attributeScores']['INSULT']['summaryScore']['value']
                         writer.writerow({'text': comment['text'], 'toxicity': comment['toxicity'], 'severe_toxicity': comment['severe_toxicity'], 'identity_attack': comment['identity_attack'], 'insult': comment['insult'], 'total_toxicity': comment['toxicity'] + comment['severe_toxicity'] + comment['identity_attack'] + comment['insult']})
                         # time.sleep(1) # necessario perchè possiamo fare solo 60 richieste al minuto ;)
-                       
+                        pbar.update(1)
+        pbar.close()
 
     with open(file_path, 'w') as f:
         json.dump(data, f, indent=4)
@@ -60,10 +64,8 @@ def main():
     
     
     file_path = 'offensive_comments/sentiment_output.json'
-    sources = get_sources(file_path)
-    print(sources)
-    for source in sources:
-        add_toxicity_to_comments(file_path, api_key, source)
+
+    add_toxicity_to_comments(file_path, api_key)
     
 
 if __name__ == "__main__":
