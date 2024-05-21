126798244e4acd7aa8c11e50d1082d2e14ace6b9
nachobascar <nachobascar@uc.cl>
2024-05-21 19:33:52 +0200

    Fix offensives

--- preprocess_sentiment_output.py
+++ preprocess_sentiment_output.py
@@ -2,7 +2,7 @@ import json
 
 from official_statistics.sentiment_total import process_post
 
-with open("sentiment_output.json", "r") as f:
+with open("sentiment_output_with_offensives.json", "r") as f:
     posts = json.load(f)
 
 with open("Recharts/src/data/sentiment_output.json", "w") as f:
