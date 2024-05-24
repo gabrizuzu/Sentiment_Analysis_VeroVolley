84b160079ac0d90969c768a3a4640571f4b70e3e
Ignacio <nachobascar@hotmail.com>
2024-05-24 09:50:54 +0200

    Add navbar

--- Recharts/src/index.js
+++ Recharts/src/index.js
@@ -31,6 +31,40 @@ const router = createBrowserRouter([
 
 root.render(
   <StrictMode>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        gap: 20,
+        backgroundColor: "lightgray",
+        padding: 10,
+      }}
+    >
+      <ul
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: 20,
+          listStyleType: "none",
+          fontSize: "1.5em",
+        }}
+      >
+        <li>
+          <a href="/">Sandbox</a>
+        </li>
+        <li>|</li>
+        <li>Layouts for Group 5:</li>
+        <li>
+          <a href="/vero-volley">Vero Volley</a>
+        </li>
+        <li>
+          <a href="/athletes">Athletes</a>
+        </li>
+        <li>
+          <a href="/offensive">Offensive</a>
+        </li>
+      </ul>
+    </nav>
     <RouterProvider router={router} />
   </StrictMode>
 );
